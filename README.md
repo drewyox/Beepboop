bd542a40b8ff910f20ecc26d02f0134c05d9642a
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-01-25 16:55:35 -0800

    add README.md

---
+++ README.md
@@ -0,0 +1,30 @@
+# _Numbers to BeepBoops_
+
+#### _Numbers to BeepBoops, 1/24/2019_
+
+#### By _**Drew Yoxsimer*_
+
+## Description
+
+_This program takes a number and prints out every integer between 0 and the user's inputted number. Every number that contains a 1 is printed as: Beep. Every number containing 2 is printed as: Boop. Every number containing 3 is printed as: I'm sorry Dave, I cant let you do that. 3,2, and 1 have priority in that respective order, ie 12 is printed as Boop, and 32 is printed as I'm sorry Dave, I cant let you do that.
+
+## Setup/Installation Requirements
+
+* _No Setup/Installation Required_
+
+_Download Bootstrap, as well as jQuery v 3.3.1.js_
+
+## Known Bugs
+
+_There are no known bugs._
+
+
+## Technologies Used
+
+_JavaScript and jQuery_
+
+### License
+
+*This Software is Licensed under the MIT License.*
+
+Copyright (c) 2019 **_Drew Yoxsimer_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,35 @@
-//
-// var results = [];
-//
-//  function beepboop(numberInput) {
-//     for (var i = 0; i <= numberInput; i++) {
-//       results.push(i);};
-//
-//     results.forEach(function(num) {
-//       var numString = num.toString();
-//       if (numString.match(/["1"]/)) {
-//           results.push("Beep");
-//         }
-//         else if (numString.match(/["2"]/)) {
-//             results.push("Boop");
-//           }
-//           else if (numString.match(/["3"]/)){
-//               results.push("I'm sorry Dave, I cant let you do that")
-//             }
-//             else {
-//                 results.push(numString);
-//
-//
-//
-//
-//
-//
-//     }
-//   });
-//     };
+var numbers = [];
+var results = [];
 
+function beepboop(numberInput) {
+  for (var i = 0; i <= numberInput; i++) {
+    numbers.push(i);};
 
-    var numbers = [];
-    var results = [];
+  numbers.forEach(function(num) {
+    var numString = num.toString();
 
-     function beepboop(numberInput) {
-        for (var i = 0; i <= numberInput; i++) {
-          numbers.push(i);};
-
-        numbers.forEach(function(num) {
-          var numString = num.toString();
-
-          if (numString.includes("1") === true && numString.includes("2") === false && numString.includes("3") === false ) {
-              results.push("Beep");
-            }
-
-          else if (numString.includes("2")===true && numString.includes("3")===false) {
-                results.push("Boop");
-            }
-
-          else if (numString.includes("3")){
-                  results.push("I'm sorry Dave, I cant let you do that");
-            }
-          else {
-            results.push(numString);
-          };
-          });
-        };
-
-
-
-
-
-
-
-
-
-
-
+    if (numString.includes("1") === true && numString.includes("2") === false && numString.includes("3") === false ) {
+        results.push("Beep ");
+      }
+    else if (numString.includes("2")===true && numString.includes("3")===false) {
+          results.push("Boop ");
+      }
+    else if (numString.includes("3")){
+            results.push("I'm sorry Dave, I cant let you do that ");
+      }
+    else {
+      results.push(numString + " ");
+    };
+  });
+};
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
       var numberInput = $("input#number").val();
-      // var truNumber = parseInt(numberInput);
-      // alert(truNumber);
       beepboop(numberInput);
-
-      console.log(results);
-
       $("#result").show();
+      $("#formOne").hide();
       $(".result").text(results);
-
-
       event.preventDefault();
     });
   });
